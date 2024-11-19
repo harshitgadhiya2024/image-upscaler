df10399d0b19f90db4dbe2c569632a00dc35f75b
Cédric Deltheil <cedric@lagon.tech>
2024-11-19 11:53:52 +0100

    refresh finegrain editor link in banner

--- src/app.py
+++ src/app.py
@@ -24,8 +24,8 @@ TITLE = """
     padding: 0.5rem 1rem;
     font-size: 1.25rem;
   ">
-    🚀 NEW: all the Finegrain spaces are now reunited under the
-    <a href="https://editor.finegrain.ai/signup?utm_source=hf&utm_campaign=image-enhancer" target="_blank">Finegrain Editor</a>. Give it a shot! 🚀
+    🚀 All the Finegrain spaces are now reunited under the
+    <a href="https://finegrain.ai/editor?utm_source=hf&utm_campaign=image-enhancer" target="_blank">Finegrain Editor</a>. Give it a shot! 🚀
   </div>
 
   <h1 style="font-size: 1.5rem; margin-bottom: 0.5rem;">
